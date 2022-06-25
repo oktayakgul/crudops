3cf31b9fbb3126b3888e9d0597eb514c93bebf01
Oktay <oktayakgul@outlook.com>
2022-06-25 19:12:50 +0300

    init

---
+++ src/test/java/com/oa/crudops/CrudopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oa.crudops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
